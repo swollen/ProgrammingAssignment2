cd58b954fce70cf0425664ff8c025395606b4899
swollen <spollenw@gmail.com>
2014-09-21 16:50:29 -0500

    R programming assignment 2, first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions to create a global variable out of an inverted matrix.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list of functions that can operate on 
+## the input matrix, including creation of a global variable that 
+## stores the solution to the inverse of the matrix. It is assumed
+## that only invertible matrices are used.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+	set <- function(y) {
+		x <<- y
+		m <<- NULL
+	}
+	get <- function() x
+	setsolve <- function(solve) m <<- solve
+	getsolve <- function() m
+	list(set = set, get = get,
+		setsolve = setsolve,
+		getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve creates the inverse of a matrix provided with the 
+## functions output by the function makeCacheMatrix.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+	m <- x$getsolve()
+	if(!is.null(m) {
+		message("getting cached data")
+		return(m)
+	}
+	data <- x$get
+	m <- solve(data, ...)
+	x$setsolve(m)
+	m
 }
